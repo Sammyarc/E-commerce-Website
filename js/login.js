a930b356525b5963192333493dac6dae31c47570
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2024-01-01 14:02:13 +0100

    modified:   js/login.js

--- js/login.js
+++ js/login.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
     const loginForm = document.getElementById("LogInForm");
-    const userField = loginForm.querySelector('.user');
-    const userInput = userField.querySelector('.username');
+    const userInput = loginForm.querySelector('.username');
+    const passInput = loginForm.querySelector('.password');
     const passField = loginForm.querySelector('.user-password');
-    const passInput = passField.querySelector('.password');
+
 
     
     const passwordInput = document.getElementById("password");
@@ -23,26 +23,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
     loginForm.addEventListener("submit", function (event) {
         event.preventDefault();
-
+    
         const users = JSON.parse(localStorage.getItem("users")) || [];
         const user = users.find(u => u.username === userInput.value && u.password === passInput.value);
-
+    
         if (user) {
             let btn = document.querySelector('.lbutton');
-                spinIcon = document.querySelector('.spinner');
-                btnText = document.querySelector(".btn-text");
+            let spinIcon = document.querySelector('.spinner');
+            let btnText = document.querySelector(".btn-text");
+    
+            btn.classList.add('checked');
+            spinIcon.classList.add('spin');
+            btnText.textContent = "Authenticating...";
+    
+            // Redirect after 5 seconds
+            setTimeout(() => {
+                window.location.href = 'index.html';
+            }, 5000);
+    
 
-                btn.classList.add('checked');
-                spinIcon.classList.add('spin');
-                btnText.textContent = "Authenticating...";
 
-                // Redirect after 3 seconds
-                setTimeout(() => {
-                    window.location.href = 'index.html';
-                }, 5000);
         } else {
             passField.classList.add("invalid");
         }
     });
+    
 });
 
