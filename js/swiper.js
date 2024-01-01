6b6f33d6d1df1ea4f05663b22f5ea6123ec21579
Owughikem Nkemakolam <nkemakolamsam@gmail.com>
2024-01-01 23:05:59 +0100

    modified:   js/swiper.js

--- js/swiper.js
+++ js/swiper.js
@@ -15,16 +15,16 @@
         autoplayHoverPause: true,
         responsive: {
             0: {
-                items: 2
+                items: 3
             },
             768: {
-                items: 3
+                items: 4
             },
             1200: {
                 items: 5
             }
         }
-    }, 800);
+    }, 500);
 
     initializeCarousel(flashCarousel, {
         loop: true,
